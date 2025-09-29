3e074bc3e600a418a8d59db4a87d38ff6f99cb67
Ihor Borunov <borunov65@gmail.com>
2025-09-29 17:56:00 +0200

    Rename test_throttle.py as tests_z_throttle.py & test_jwt.py as tests_jwt.py

--- app/settings.py
+++ app/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/min", "user": "30/min"},
 }
 
 SPECTACULAR_SETTINGS = {
