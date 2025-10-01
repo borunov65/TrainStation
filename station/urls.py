11f219d90218e51d8df116b544d01b8934503d63
Ihor Borunov <borunov65@gmail.com>
2025-10-01 19:19:16 +0200

    Add routers for crews, tickets, routes, stations and cargos

--- station/urls.py
+++ station/urls.py
@@ -1,14 +1,29 @@
 from django.urls import path, include
-from station.views import TrainTypeViewSet, TrainViewSet, JourneyViewSet, OrderViewSet
+from station.views import (
+    TrainTypeViewSet,
+    TrainViewSet,
+    JourneyViewSet,
+    OrderViewSet,
+    CrewViewSet,
+    TicketViewSet,
+    RouteViewSet,
+    StationViewSet,
+    CargoViewSet
+)
 from rest_framework import routers
 
 app_name = "station"
 
 router = routers.DefaultRouter()
-router.register("trains", TrainViewSet)
-router.register("journeys", JourneyViewSet)
 router.register("train_types", TrainTypeViewSet)
+router.register("trains", TrainViewSet)
+router.register("crews", CrewViewSet)
+router.register("cargos", CargoViewSet)
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+router.register("journeys", JourneyViewSet)
 router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
