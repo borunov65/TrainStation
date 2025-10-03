4ff1edab2a64c93a2fcd012a74a3e0391fd778dd
Ihor Borunov <borunov65@gmail.com>
2025-10-03 11:14:13 +0200

    Remove the secret_key from the settings.py file

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&@fw@^%c$7sr8pmhs+$=pn7942n3xfx0$4^oiopjy_p%%$@fa7"
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
